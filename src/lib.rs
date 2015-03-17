06df32ac940345f90a86bc8090acac7894656480
Chris Campbell <campbell@plausible.coop>
2015-03-17 16:29:41 -0700

    Sketch out a heap-backed storage type.

--- src/lib.rs
+++ src/lib.rs
@@ -7,26 +7,18 @@
 //
 
 /// An immutable vector of bytes.
-pub trait ByteVector {
-
-    /// Return the length, in bytes.
-    fn length(&self) -> u64;
-}
-
-/// ByteVector implementation.
-// TODO: Only public because of type declaration issue below
-pub struct ByteVectorImpl {
+pub struct ByteVector {
     /// The underlying storage type.
     storage: StorageType
 }
 
-impl ByteVector for ByteVectorImpl {
-
-    // From ByteVector trait
+impl ByteVector {
+    /// Return the length, in bytes.
     fn length(&self)-> u64 {
         match self.storage {
             StorageType::Empty => 0,
-//            StorageType::DirectValue => 4,
+            StorageType::Heap { ref bytes } => bytes.len() as u64,
+//            StorageType::Direct { len, bytes } => len
         }
     }
 }
@@ -34,16 +26,23 @@ impl ByteVector for ByteVectorImpl {
 /// A sum type over all supported storage object types.
 enum StorageType  {
     Empty,
-//    DirectValue// { v: Int }
+    Heap { bytes: std::vec::Vec<u8> },
+//    Direct { len: u64, bytes: [u8, ..8] }
 }
 
 /// An empty byte vector.
-// TODO: Is it possible to declare this as the trait type?
-pub static EMPTY: ByteVectorImpl = ByteVectorImpl { storage: StorageType::Empty };
+pub static EMPTY: ByteVector = ByteVector { storage: StorageType::Empty };
 
-///// Byte vector that directly stores a certain number of bytes.
+/// Return a byte vector that stores a certain number of bytes on the heap.
+pub fn buffered(bytes: Vec<u8>) -> ByteVector {
+    let storage = StorageType::Heap { bytes: bytes };
+    ByteVector { storage: storage }
+}
+
+/// Return a byte vector that directly stores a certain number of bytes on the stack.
 //pub fn direct<T>(v: T) -> ByteVector {
-//    EMPTY;
+//    let storage = StorageType::Direct { len: 1, bytes: [0u8, ..8] };
+//    ByteVectorImpl { storage: storage }
 //}
 
 #[cfg(test)]
@@ -55,7 +54,13 @@ mod tests {
         assert_eq!(EMPTY.length(), 0);
     }
 
-//    #[test]
+    #[test]
+    fn length_of_buffered_vector_should_be_correct() {
+        let bytes = vec![1, 2, 3, 4];
+        assert_eq!(buffered(bytes).length(), 4);
+    }
+
+    //    #[test]
 //    fn length_of_direct_vector_should_be_size_of_integer() {
 //        assert_eq!(direct(7u8).length(), 1);
 //    }
