8fab489419e00a972759393172eeec60ead37f89
Chris Campbell <campbell@plausible.coop>
2015-03-23 12:24:07 -0700

    Simplify codec definitions by dropping Encoder/Decoder traits and instead pass boxed closures directly.

--- src/codec.rs
+++ src/codec.rs
@@ -9,34 +9,28 @@
 use error::Error;
 use byte_vector;
 use byte_vector::ByteVector;
-use hlist;
 use hlist::*;
 
 /// Implements encoding and decoding of values of type `T`.
 #[allow(dead_code)]
 pub struct Codec<T> {
-    encoder: Box<Encoder<T>>,
-    decoder: Box<Decoder<T>>
+    encoder: Box<Fn(&T) -> EncodeResult>,
+    decoder: Box<Fn(&ByteVector) -> DecodeResult<T>>
 }
 
 #[allow(dead_code)]
 impl<T> Codec<T> {
     fn encode(&self, value: &T) -> EncodeResult {
-        self.encoder.encode(value)
+        (*self.encoder)(value)
     }
 
     fn decode(&self, bv: &ByteVector) -> DecodeResult<T> {
-        self.decoder.decode(bv)
+        (*self.decoder)(bv)
     }
 }
 
 /// A result type returned by Encoder operations.
 type EncodeResult = Result<ByteVector, Error>;
-
-/// Implements encoding a value of type `T` to a ByteVector.
-trait Encoder<T> {
-    fn encode(&self, value: &T) -> EncodeResult;
-}
 
 /// A result type, consisting of a decoded value and any unconsumed data, returned by Decoder operations.
 #[allow(dead_code)]
@@ -48,42 +42,39 @@ pub struct DecoderResult<T> {
 /// A result type returned by Decoder operations.
 type DecodeResult<T> = Result<DecoderResult<T>, Error>;
 
-/// Implements decoding a value of type `T` from a ByteVector.
-trait Decoder<T> {
-    fn decode(&self, bv: &ByteVector) -> DecodeResult<T>;
-}
-
-/// XXX: Rough sketch of an integral codec, currently only defined for u8 type
-struct IntegralEncoder;
-impl Encoder<u8> for IntegralEncoder {
-    fn encode(&self, value: &u8) -> EncodeResult {
-        // TODO: Use direct() once it's implemented
-        Ok(byte_vector::buffered(&vec![*value]))
-    }
-}
-
-struct IntegralDecoder;
-impl Decoder<u8> for IntegralDecoder {
-    fn decode(&self, bv: &ByteVector) -> DecodeResult<u8> {
-        // TODO: This is a dumbed down implementation just for evaluation purposes
-        let v: &mut[u8] = &mut[0];
-        match bv.read(v, 0, 1) {
-            Ok(..) => {
-                match bv.drop(1) {
-                    Ok(remainder) => Ok(DecoderResult { value: v[0], remainder: remainder }),
-                    Err(e) => Err(e)
-                }
-            },
-            Err(e) => Err(e)
-        }
-    }
-}
-
 /// Unsigned 8-bit integer codec.
 pub fn uint8() -> Codec<u8> {
     Codec {
-        encoder: Box::new(IntegralEncoder),
-        decoder: Box::new(IntegralDecoder)
+        encoder: Box::new(|value| {
+            // TODO: Use direct() once it's implemented
+            Ok(byte_vector::buffered(&vec![*value]))
+        }),
+        decoder: Box::new(|bv| {
+            // TODO: This is a dumbed down implementation just for evaluation purposes
+            let v: &mut[u8] = &mut[0];
+            match bv.read(v, 0, 1) {
+                Ok(..) => {
+                    match bv.drop(1) {
+                        Ok(remainder) => Ok(DecoderResult { value: v[0], remainder: remainder }),
+                        Err(e) => Err(e)
+                    }
+                },
+                Err(e) => Err(e)
+            }
+        })
+    }
+}
+
+#[allow(unused_variables)]
+pub fn hnil_codec() -> Codec<HNil> {
+    Codec {
+        encoder: Box::new(|value| {
+            Ok(byte_vector::empty())
+        }),
+        decoder: Box::new(|bv| {
+            // TODO: Can we avoid creating a view here?  Or at least make ByteVector implement Clone?
+            bv.drop(0).map(|rem| DecoderResult { value: HNil, remainder: rem })
+        })
     }
 }
 
@@ -109,33 +100,10 @@ pub fn uint8() -> Codec<u8> {
 //     }
 // }
 
-struct HNilEncoder;
-impl Encoder<HNil> for HNilEncoder {
-    fn encode(&self, value: &HNil) -> EncodeResult {
-        Ok(byte_vector::empty())
-    }
-}
-
-struct HNilDecoder;
-impl Decoder<HNil> for HNilDecoder {
-    fn decode(&self, bv: &ByteVector) -> DecodeResult<HNil> {
-        // TODO: This creates an unnecessary view
-        bv.drop(0).map(|rem| DecoderResult { value: HNil, remainder: rem })
-    }
-}
-
-pub fn hnil_codec() -> Codec<HNil> {
-    Codec {
-        encoder: Box::new(HNilEncoder),
-        decoder: Box::new(HNilDecoder)
-    }
-}
-
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::fmt::Debug;
-    use std::intrinsics::*;
     use error::Error;
     use byte_vector;
     use byte_vector::ByteVector;
