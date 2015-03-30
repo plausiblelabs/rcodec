68113acb1ea97b934b7adc310cf3dc7635dc53ef
Chris Campbell <campbell@plausible.coop>
2015-03-30 10:27:36 -0700

    Drop some hlist-related macros that are not currently needed.

--- src/macros.rs
+++ src/macros.rs
@@ -15,57 +15,11 @@ macro_rules! hlist {
     {} => {
         $crate::hlist::HNil
     };
-    // TODO: What does this rule do?
-    {=> $($elem:tt)+ } => {
-        hlist_pat!($($elem)+)
-    };
     { $head:expr } => {
         $crate::hlist::HCons($head, $crate::hlist::HNil)
     };
     { $head:expr, $($tail:expr),+ } => {
         $crate::hlist::HCons($head, hlist!($($tail),+))
-    };
-    { $($head:expr),+ : $tail:expr } => {
-        hlist_expr_tail!({ $tail } $($head),+)
-    };
-}
-
-macro_rules! hlist_expr_tail {
-    { { $tail:expr } } => {
-        $tail
-    };
-    { { $tail:expr } $head:expr } => {
-        $crate::hlist::HCons($head, $tail)
-    };
-    { { $tail:tt } $head:expr, $($rest:tt),+ } => {
-        $crate::hlist::HCons($head, hlist_expr_tail!({ $tail } $($rest),+))
-    };
-}
-
-macro_rules! hlist_pat {
-    {} => {
-        $crate::hlist::HNil
-    };
-    { $head:pat } => {
-        $crate::hlist::HCons($head, $crate::hlist::HNil)
-    };
-    { $head:pat, $($rest:tt),+ } => {
-        $crate::hlist::HCons($head, hlist_pat!($($rest),+))
-    };
-    { $($head:pat),+ : $tail:pat } => {
-        hlist_pat_tail!({ $tail } $($head),+)
-    };
-}
-
-macro_rules! hlist_pat_tail {
-    { { $tail:pat } } => {
-        $tail
-    };
-    { { $tail:pat } $head:pat } => {
-        $crate::hlist::HCons($head, $tail)
-    };
-    { { $tail:tt } $head:pat, $($rest:tt),+ } => {
-        $crate::hlist::HCons($head, hlist_pat_tail!({ $tail } $($rest),+))
     };
 }
 
